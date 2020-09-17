5c2b24e36a0875661c0abbdf7bb019288ce73341
Jawad Shafiq <30720322+jawad-nafad@users.noreply.github.com>
2020-09-18 03:58:51 +1000

    Update Theory.js

--- src/components/Theory.js
+++ src/components/Theory.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";  
 import Readpdf from '../readpdf';
-//import samplePDF from '../asset/sample.pdf'; //pdf is stored in this app
-import samplePDF from 'https://drive.google.com/file/d/13Er-MvYVrnULmxOtQ2Da0GJoEjbVBZN_/view?usp=sharing';
+import samplePDF from '../asset/sample.pdf'; //pdf is stored in this app
+
 
 //this calls ReadPdf module to view the pdf file, can be updated future with more data/files
 export default function Theory(){
