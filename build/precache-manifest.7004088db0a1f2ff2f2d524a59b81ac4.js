f12f26162d837a98953c42672b3541b0d4d81078
jawad <jawadalnafad@gmail.com>
2020-09-18 04:42:38 +1000

    ssss

---
+++ build/precache-manifest.7004088db0a1f2ff2f2d524a59b81ac4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef2fab3c004be0faf02b3900a01a84f",
+    "url": "/quiz_app/index.html"
+  },
+  {
+    "revision": "d9577c835d417f2a6a59",
+    "url": "/quiz_app/static/css/main.9f282d92.chunk.css"
+  },
+  {
+    "revision": "4cb1085999a1890b63ea",
+    "url": "/quiz_app/static/js/2.1929681f.chunk.js"
+  },
+  {
+    "revision": "2927c17f155caba9c734acdc50e6f829",
+    "url": "/quiz_app/static/js/2.1929681f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9577c835d417f2a6a59",
+    "url": "/quiz_app/static/js/main.e917814d.chunk.js"
+  },
+  {
+    "revision": "e899a3e6563f252a6241",
+    "url": "/quiz_app/static/js/runtime-main.905b4f5f.js"
+  },
+  {
+    "revision": "51029baf1cdefd8e02e16b98c7dd1d00",
+    "url": "/quiz_app/static/media/sample.51029baf.pdf"
+  }
+]);
